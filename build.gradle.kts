e02f587e51326ec0a22f232ebe7f1c255b46c8fc
Nussipzhan <nusip.maks@gmail.com>
2024-06-21 02:15:44 +0500

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,67 @@
+val swaggerVersion = "2.9.2"
+val h2Version = "1.4.200"
+val jacksonVersion = "2.10.2"
+val jjwtVersion = "0.9.1"
+val junitVersion = "5.5.2"
+val junitPlatformVersion = "1.5.2"
+val mockkVersion = "1.9.3"
+
+plugins {
+    kotlin("jvm") version "1.8.0"
+    kotlin("plugin.spring") version "1.8.0"
+    id("org.springframework.boot") version "2.7.0"
+    id("io.spring.dependency-management") version "1.0.11.RELEASE"
+    application
+}
+
+group = "kz.maks"
+version = "1.0-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_17
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-security")
+
+    implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-xml:$jacksonVersion")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:$jacksonVersion")
+
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+
+    implementation("io.springfox:springfox-swagger2:$swaggerVersion")
+    implementation("io.springfox:springfox-swagger-ui:$swaggerVersion")
+
+    implementation("io.jsonwebtoken:jjwt:$jjwtVersion")
+
+    runtimeOnly("org.postgresql:postgresql")
+
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.springframework.security:spring-security-test")
+    testImplementation(kotlin("test"))
+
+    testImplementation("org.junit.jupiter:junit-jupiter-api:$junitVersion")
+    testImplementation("org.junit.jupiter:junit-jupiter-params:$junitVersion")
+    testImplementation("org.junit.platform:junit-platform-commons:$junitPlatformVersion")
+    testImplementation("org.junit.platform:junit-platform-launcher:$junitPlatformVersion")
+    testImplementation("io.mockk:mockk:$mockkVersion")
+
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:$junitVersion")
+    testRuntimeOnly("com.h2database:h2:$h2Version")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+//kotlin {
+//    jvmToolchain(11)
+//}
+
+application {
+    mainClass.set("MainKt")
+}
---
+++ src/main/kotlin/kz/maks/paymenttesttask/App.kt
@@ -0,0 +1,10 @@
+package kz.maks.paymenttesttask
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PaymentTestApp
+fun main(args: Array<String>) {
+    runApplication<PaymentTestApp>(*args)
+}
