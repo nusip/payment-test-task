56f428ed288e133a97b8c4fe5af88fb13b5485f9
Nussipzhan <nusip.maks@gmail.com>
2024-06-25 21:38:51 +0500

    Remove redundant from SecurityConfig

--- src/main/kotlin/kz/maks/paymenttesttask/auth/SecurityConfig.kt
+++ src/main/kotlin/kz/maks/paymenttesttask/auth/SecurityConfig.kt
@@ -1,10 +1,7 @@
-package kz.astonline.myhealth.auth
+package kz.maks.paymenttesttask.auth
 
 import kz.maks.paymenttesttask.auth.jwt.JwtAuthConfigurer
 import kz.maks.paymenttesttask.auth.jwt.JwtAuthProvider
-import org.slf4j.LoggerFactory
-import org.springframework.beans.factory.annotation.Value
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.authentication.AuthenticationManager
@@ -13,9 +10,6 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 import org.springframework.security.config.http.SessionCreationPolicy
-import org.springframework.web.cors.CorsConfiguration
-import org.springframework.web.cors.CorsConfigurationSource
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource
 
 @Configuration
 @EnableWebSecurity
@@ -24,46 +18,9 @@ class SecurityConfig(
     private val tokenProvider: JwtAuthProvider
 ) : WebSecurityConfigurerAdapter() {
 
-    private val log = LoggerFactory.getLogger(SecurityConfig::class.java)
-
     @Bean
     override fun authenticationManagerBean(): AuthenticationManager {
         return super.authenticationManagerBean()
-    }
-
-    @Value("\${security.cors.enabled}")
-    private val corsEnabled = false
-
-    @Value("\${security.cors.origins}")
-    private lateinit var corsAllowedOrigins: List<String>
-
-    @Value("\${security.cors.methods}")
-    private lateinit var corsAllowedMethods: List<String>
-
-    @Value("\${security.cors.headers}")
-    private lateinit var corsAllowedHeaders: List<String>
-
-    @Value("\${security.cors.credentials}")
-    private val corsAllowCredentials = false
-
-    @Bean
-    @ConditionalOnProperty(name = ["security.cors.enabled"], havingValue = "true")
-    fun corsConfigurationSource(): CorsConfigurationSource {
-        val source = UrlBasedCorsConfigurationSource()
-        log.info("cors.enabled = $corsEnabled")
-        log.info("cors.origins = ${corsAllowedOrigins.joinToString()}")
-        log.info("cors.methods = ${corsAllowedMethods.joinToString()}")
-        log.info("cors.headers = ${corsAllowedHeaders.joinToString()}")
-        log.info("cors.credentials = $corsAllowCredentials")
-
-        val corsConfig = CorsConfiguration()
-        corsConfig.allowedOrigins = corsAllowedOrigins
-        corsConfig.allowedMethods = corsAllowedMethods
-        corsConfig.allowedHeaders = corsAllowedHeaders
-        corsConfig.allowCredentials = corsAllowCredentials
-        source.registerCorsConfiguration("/**", corsConfig)
-
-        return source
     }
 
     override fun configure(http: HttpSecurity) {
